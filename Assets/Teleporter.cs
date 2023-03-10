9d1ac8bf5fa038da6924cfcbd2f909b426f4d4aa
Lam Ky Anh Do <faleanh@hotmail.com>
2023-03-10 12:05:52 -0500

    Debugged for a playable playtest

--- Assets/Teleporter.cs
+++ Assets/Teleporter.cs
@@ -9,6 +9,15 @@ public class Teleporter : MonoBehaviour
     public GameObject ToDoorTwo;
     public GameObject ToDoorThree;
     public GameObject ToDoorFour;
+    public GameObject ToDoorFive;
+    public GameObject ToDoorSix;
+    public GameObject ToDoorSeven;
+    public GameObject ToDoorEight;
+    public GameObject ToDoorNine;
+    public GameObject ToDoorTen;
+    public GameObject ToDoorEleven;
+    public GameObject ToDoorTwelve;
+    public GameObject ToGoldDoor;
 
     //public AudioSource TeleportSound;
 
@@ -22,7 +31,7 @@ public class Teleporter : MonoBehaviour
 
         if (collision.gameObject.CompareTag("DoorTwo"))
         {
-            Player.transform.position = ToDoorThree.transform.position;
+            Player.transform.position = ToDoorSix.transform.position;
             //TeleportSound.Play();
         }
 
@@ -34,7 +43,59 @@ public class Teleporter : MonoBehaviour
 
         if (collision.gameObject.CompareTag("DoorFour"))
         {
+            Player.transform.position = ToDoorNine.transform.position;
+            //TeleportSound.Play();
+        }
+        if (collision.gameObject.CompareTag("DoorFive"))
+        {
             Player.transform.position = ToDoorOne.transform.position;
+            //TeleportSound.Play();
+        }
+
+        if (collision.gameObject.CompareTag("DoorSix"))
+        {
+            Player.transform.position = ToDoorFour.transform.position;
+            //TeleportSound.Play();
+        }
+
+        if (collision.gameObject.CompareTag("DoorSeven"))
+        {
+            Player.transform.position = ToDoorTwelve.transform.position;
+            //TeleportSound.Play();
+        }
+
+        if (collision.gameObject.CompareTag("DoorEight"))
+        {
+            Player.transform.position = ToDoorEight.transform.position;
+            //TeleportSound.Play();
+        }
+        if (collision.gameObject.CompareTag("DoorNine"))
+        {
+            Player.transform.position = ToDoorFive.transform.position;
+            //TeleportSound.Play();
+        }
+
+        if (collision.gameObject.CompareTag("DoorTen"))
+        {
+            Player.transform.position = ToDoorEleven.transform.position;
+            //TeleportSound.Play();
+        }
+
+        if (collision.gameObject.CompareTag("DoorEleven"))
+        {
+            Player.transform.position = ToDoorNine.transform.position;
+            //TeleportSound.Play();
+        }
+
+        if (collision.gameObject.CompareTag("DoorTwelve"))
+        {
+            Player.transform.position = ToDoorTen.transform.position;
+            //TeleportSound.Play();
+        }
+
+        if (collision.gameObject.CompareTag("GoldDoor"))
+        {
+            Player.transform.position = ToGoldDoor.transform.position;
             //TeleportSound.Play();
         }
     }
